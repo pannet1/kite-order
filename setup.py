b856863c5e5fd4c72029b9de0e6c3adbf8b97a88
karthick <prog@ecomsense.in>
2023-03-14 10:07:14 +0530

    fix url setup.py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ setup(name='kite-order',
       description='a portfolio manager that deals with orders based on cancelled orders',
       author='b karthick',
       author_email='prog@ecomsense.in',
-      url='https://github.com/pannet1/dealer-web/',
-      packages=['dealer-web']
+      url='https://github.com/pannet1/kite-order/',
+      packages=['kite-order']
       )
