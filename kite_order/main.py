4d23c5e51a36badd223905a67c45bdc92d7ce7cc
karthick <prog@ecomsense.in>
2023-03-30 23:37:38 +0530

    add paper test

--- kite_order/main.py
+++ kite_order/main.py
@@ -7,15 +7,11 @@ from login_get_kite import get_kite
 from typing import Dict
 
 WORK_PATH = "../../confid/"
-# logging = Logger(20, WORK_PATH + 'kite_order.log')
-logging = Logger(10)
-api = ""  # "" is zerodha, optional bypass
-z = get_kite(api, WORK_PATH)
-
+logging = Logger(20, WORK_PATH + 'kite_order.log')
+df_null = pd.DataFrame()
 f = Fileutils()
 MIS = f.get_lst_fm_yml('MIS.yaml')
 NRML = f.get_lst_fm_yml('NRML.yaml')
-
 comk = ['exchange',
         'symbol',
         'product',
@@ -28,12 +24,12 @@ comk = ['exchange',
         'order_type',
         'average_price',
         'last_price']
-
 ordk = ['symbol',
         'product',
         'order_id',
         'order_type',
         'status']
+pd.options.mode.chained_assignment = None
 
 
 def chek_stop(row: pd.Series) -> float:
@@ -56,7 +52,7 @@ def myround(x, prec=2, base=.05):
     return round(base * round(float(x)/base), prec)
 
 
-def ordr_mgmt(dct_ordr: Dict, ops: str) -> None:
+def ordr_mgmt(dct_ordr: Dict, ops: str, z) -> None:
     try:
         dct_ordr.pop('average_price')
         dct_ordr.pop('last_price')
@@ -83,9 +79,6 @@ def ordr_mgmt(dct_ordr: Dict, ops: str) -> None:
         return
 
 
-df_null = pd.DataFrame()
-
-
 def get_orders(lst_all_ords):
     df_trgt = df_stop = df_null
     if any(lst_all_ords):
@@ -96,7 +89,6 @@ def get_orders(lst_all_ords):
         df_ords['status'] = df_ords['status'].replace([None], 'UNKNOWN')
         df_ords = df_ords.filter(ordk)
         print(df_ords, "\n")
-        # filter dataframes based on order types
         df_stop = df_ords.query("order_type=='SL'").copy()
         df_trgt = df_ords.query(
             "order_type=='MARKET' and status=='REJECTED'").copy()
@@ -106,7 +98,6 @@ def get_orders(lst_all_ords):
 def get_positions(lst_all_posn):
     df_pos = df_null
     if any(lst_all_posn):
-        NOT_MOD = True
         filr_pos = pd.DataFrame.from_records(
             lst_all_posn)
         filr_pos = filr_pos.query("exchange=='NFO'")
@@ -135,30 +126,30 @@ def merg_pos_stop(df_pos, df_stop):
     return posn_stop
 
 
-def is_not_order_dirty(posn_stop):
+def is_not_order_dirty(posn_stop, z):
     NOT_MOD = True
     for i, o in posn_stop.iterrows():
         """
         Positions without SL contains order_type as nan
         """
         if o['order_type'] != 'SL' and o['sqof'] <= 0:
-            ordr_mgmt(o, 'SL')
+            ordr_mgmt(o, 'SL', z)
             logging.info(f"placed stop for symbol {o['symbol']}")
             NOT_MOD = False
         elif o['order_type'] != 'SL' and o['sqof'] > 0:
-            ordr_mgmt(o, 'MARKET')
+            ordr_mgmt(o, 'MARKET', z)
             logging.info(
                 f"squaring {o['symbol']} in loss but without order")
             NOT_MOD = False
         elif o['order_type'] == 'SL' and o['sqof'] > 0:
             logging.info(
                 f"modified {o['order_id']} for {o['symbol']} in loss")
-            ordr_mgmt(o, 'MODIFY')
+            ordr_mgmt(o, 'MODIFY', z)
             NOT_MOD = False
     return NOT_MOD
 
 
-def do_targets(posn_stop, df_trgt):
+def do_targets(posn_stop, df_trgt, z):
     stop = posn_stop.query("order_type=='SL'")
     stop = stop.filter(ordk).drop(["status", "order_type"], axis=1)
 
@@ -176,23 +167,25 @@ def do_targets(posn_stop, df_trgt):
         o['sqof'] = 0
         o['pric'] = 0
         o['trgr'] = 0
-        ordr_mgmt(o, 'TARGET')
+        ordr_mgmt(o, 'TARGET', z)
         logging.info(f"target reached for {o['symbol']}")
 
 
-pd.options.mode.chained_assignment = None
-while True:
-    try:
-        print("ORDERS \n")
-        df_stop, df_trgt = get_orders(z.orders)
-        print("\n POSITIONS")
-        df_pos = get_positions(z.positions)
-        print("\n POSITIONS and STOPS")
-        posn_stop = merg_pos_stop(df_pos, df_stop)
-        print("\n POSITION and TARGETS")
-        if is_not_order_dirty(posn_stop):
-            do_targets(posn_stop, df_trgt)
-    except Exception as e:
-        logging.error(f"error {e} occured")
-    finally:
-        Utilities().slp_til_nxt_sec()
+if __name__ == "__main__":
+    api = ""  # "" is zerodha, optional bypass
+    z = get_kite(api, WORK_PATH)
+    while True:
+        try:
+            print("ORDERS \n")
+            df_stop, df_trgt = get_orders(z.orders)
+            print("\n POSITIONS")
+            df_pos = get_positions(z.positions)
+            print("\n POSITIONS and STOPS")
+            posn_stop = merg_pos_stop(df_pos, df_stop)
+            print("\n POSITION and TARGETS")
+            if is_not_order_dirty(posn_stop, z):
+                do_targets(posn_stop, df_trgt, z)
+        except Exception as e:
+            logging.error(f"error {e} occured")
+        finally:
+            Utilities().slp_til_nxt_sec()
---
+++ kite_order/paper.py
@@ -0,0 +1,10 @@
+from pprint import pprint
+
+
+class Paper:
+
+    def order_modify(self, **dct):
+        pprint(dct)
+
+    def order_place(self, **dct):
+        pprint(dct)
